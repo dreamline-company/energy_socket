5a20f72931f194f70f5b040b8b60ee0e18d6e6dd
Amirkhan Orazbay <amirhan9878@gmail.com>
2023-03-05 17:54:51 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("64.226.72.254", 80))
+sock.connect(("64.226.72.254", 8080))
 sock.send(b"GET / HTTP/1.1\r\nHost:64.226.72.254\r\n\r\n")
 response = sock.recv(4096)
 sock.close()
