913cc10f8c57e5b5d2d703c81d85834dece33f7f
Amirkhan <amirhan9878@gmail.com>
2023-05-05 07:53:04 +0600

    OK Recv

--- client.py
+++ client.py
@@ -1,8 +1,30 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("64.226.72.254", 8080))
-sock.send(b"GET / HTTP/1.1\r\nHost:64.226.72.254\r\n\r\n")
-response = sock.recv(4096)
-sock.close()
-print(response.decode())
+sock.connect(("0.0.0.0", 8080))
+
+def sendRealTime(): 
+    sock.send(ord('<').to_bytes(1, 'little'))#start of msg 0
+    sock.send(int('0',16).to_bytes(1, 'little'))#type of msg 1
+    sock.send(int('1',16).to_bytes(1, 'little'))#controller_id 2
+    sock.send(int('AB',16).to_bytes(1, 'little'))#slave_id 3
+    sock.send(int('1',16).to_bytes(1, 'little'))#register hi 4
+    sock.send(int('25',16).to_bytes(1, 'little'))#register lo 5
+    sock.send(int('AB',16).to_bytes(1, 'little'))#value 6
+    sock.send(int('37',16).to_bytes(1, 'little'))#value 7
+    sock.send(ord('>').to_bytes(1, 'little'))#end of msg 8
+    
+def sendCtrl():
+    sock.send(ord('<').to_bytes(1, 'little'))#start of msg 0
+    sock.send(int('1',16).to_bytes(1, 'little'))#type of msg 1
+    sock.send(int('4',16).to_bytes(1, 'little'))#controller_id 2
+    sock.send(int('3',16).to_bytes(1, 'little'))#current_a 3
+    sock.send(int('4',16).to_bytes(1, 'little'))#current_b 4
+    sock.send(int('5',16).to_bytes(1, 'little'))#current_c 5
+    sock.send(ord('>').to_bytes(1, 'little'))#end of msg 6
+try:
+    sendCtrl()
+except Exception as e:
+    print(e)
+
+print(sock.recv(1024))
--- server.py
+++ server.py
@@ -1,12 +1,61 @@
-# echo-server.py
-
 import socket
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+from datetime import datetime
 
-HOST = "64.226.72.254"  # Standard loopback interface address (localhost)
-PORT = 8080  # Port to listen on (non-privileged ports are > 1023)
+HOST = "0.0.0.0"
+PORT = 8080  
+IP = socket.gethostbyname(HOST)
 
+print(f"Hostname: {HOST}")
+print(f"IP Address: {IP}")
+
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database='defaultdb',
+            port=25060,
+            ssl_ca='/Users/amirkhanorazbay/Downloads/ca-certificate (1).crt'
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+def insert_realtime_data(data): 
+    cnx = create_server_connection("db-mysql-fra1-01434-do-user-13902982-0.b.db.ondigitalocean.com", "doadmin", "AVNS_YJZHKISaSzqXCi6aSRo")
+    cursor = cnx.cursor()
+    insert_realtime = "INSERT INTO realtime (controller_id, slave_id, register, value) VALUES (%s, %s, %s, %s)"
+    print(data)
+    # Insert new employee
+    cursor.execute(insert_realtime, data)
+    # Make sure data is committed to the database
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+def insert_controller_data(data): 
+    cnx = create_server_connection("db-mysql-fra1-01434-do-user-13902982-0.b.db.ondigitalocean.com", "doadmin", "AVNS_YJZHKISaSzqXCi6aSRo")
+    cursor = cnx.cursor()
+    insert_controller = ("INSERT INTO controller "
+                "(controller_id, current_a, current_b, current_c, timestamp) "
+                "VALUES (%s, %s, %s, %s, %s)")
+    # Insert new employee
+    cursor.execute(insert_controller, data)
+    # Make sure data is committed to the database
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+    s.bind((IP, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
@@ -15,4 +64,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
-            conn.sendall(data)
+            if data[0] == 60:
+                if data[1] == 0:
+                    if data[len(data) - 1] == 62:
+                        calc = data[len(data) - 2]
+                        for i in range (len(data) - 8):
+                            calc |= data[6 + i] << (8 ** (i+1))
+                        #register to dec data[4] << 8 | data[5]
+                        s = (data[2], data[3], hex(data[4] << 8 | data[5]), calc)
+                        print(s)
+                        insert_realtime_data(s)
+                elif data[1] == 1:
+                    if data[len(data) - 1] == 62:
+                        #register to dec data[4] << 8 | data[5]
+                        now = datetime.now()
+                        formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
+                        s = (data[2], data[3], data[4], data[5], formatted_date)
+                        print(s)
+                        insert_controller_data(s)
+            conn.sendall(b"OK!Recv")
