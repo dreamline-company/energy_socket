26b2837fb6e6ddf76de74efecbe2195821d9f7a7
Amirkhan <amirhan9878@gmail.com>
2023-05-06 03:00:59 +0600

    sfaef

--- server.py
+++ server.py
@@ -64,6 +64,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if not data:
                 break
+            print(data)
             if data[0] == 60:
                 if data[1] == 0:
                     if data[len(data) - 1] == 62:
