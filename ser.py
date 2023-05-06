67e6b1fa58a42510b4ba78c1974b89872b993bef
Amirkhan <amirhan9878@gmail.com>
2023-05-06 21:55:48 +0600

    aDAWDA

--- ser.py
+++ ser.py
@@ -3,26 +3,39 @@ import mysql.connector
 from mysql.connector import Error
 import pandas as pd
 from datetime import datetime
+import os
+from _thread import *
+
+def multi_threaded_client(connection):
+    connection.send(str.encode('Server is working:'))
+    while True:
+        data = connection.recv(2048)
+        response = 'Server message: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(response))
+    connection.close()
 
 HOST = '167.71.43.13'
 PORT = 8080  
 IP = socket.gethostbyname(HOST)
+ServerSideSocket = socket.socket()
+ThreadCount = 0
 
 print(f"Hostname: {HOST}")
 print(f"IP Address: {IP}")
 
-    
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((IP, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        count = 0
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(data)
-            print(data)
-            conn.sendall(b"OK!Recv")
+try:
+    ServerSideSocket.bind((HOST, PORT))
+except socket.error as e:
+    print(str(e))
+print('Socket is listening..')
+ServerSideSocket.listen(5)
+
+while True:
+    Client, address = ServerSideSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(multi_threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+ServerSideSocket.close()
