90cfacfa8ab7dc543a179b69918187ac9e0ea476
Amirkhan <amirhan9878@gmail.com>
2023-05-24 10:40:32 +0600

    fsefawe

--- server.py
+++ server.py
@@ -97,7 +97,7 @@ def multi_threaded_client(connection, address):
                         print(s)
                         insert_realtime_data(s)
                     else:
-                        s = (hex(object_number), now, now, data[3], temp, voltage, temp_cpu, data[7], data[8] << 8 | data[9]) + tuple(data[10:21])
+                        s = (hex(object_number), now, now, data[3], temp, voltage, temp_cpu, data[7], hex(data[8] << 8 | data[9]))
                         print(s)
                         insert_realtime_data(s)
                 elif data[1] == 2:
