b2078903dbd82491c1c465a0241580fb8b055d14
Amirkhan <amirhan9878@gmail.com>
2023-05-29 13:32:18 +0600

    optimize

--- server.py
+++ server.py
@@ -98,11 +98,12 @@ def multi_threaded_client(connection, address):
                     temp = data[4]
                     voltage = data[5]
                     temp_cpu = data[6]
+                    cell_number = hex(data[8] << 8 | data[9])
                     if (data[8] << 8 | data[9]) < 256:
-                        s = (hex(object_number), now, now, float(temp), float(voltage), float(temp_cpu), data[7], hex(data[8] << 8 | data[9])), get_binary(data[10:14])
+                        s = (hex(object_number), now, now, float(temp), float(voltage), float(temp_cpu), data[7], cell_number, get_binary(data[10:14]))
                         insert_regular_table_data(s)
                     else:
-                        s = (hex(object_number), now, now, float(temp), float(voltage), float(temp_cpu), data[7], hex(data[8] << 8 | data[9]), data[10])
+                        s = (hex(object_number), now, now, float(temp), float(voltage), float(temp_cpu), data[7], cell_number, data[10])
                         insert_regular_table_data(s)
                 elif data[1] == 2:
                     #register to dec data[4] << 8 | data[5]
