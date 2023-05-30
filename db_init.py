17800968c8f844fafd5af5832d11b35f9a258966
Amirkhan <amirhan9878@gmail.com>
2023-05-30 18:18:13 +0600

    input_state s number reduce to 8

--- db_init.py
+++ db_init.py
@@ -24,9 +24,9 @@ def create_server_connection(host_name, user_name, user_password):
 
 cnx = create_server_connection("46.101.102.163", "root", "my-secret-pw")
 cursor = cnx.cursor()
-main_var = ["id INT AUTO_INCREMENT PRIMARY KEY", "object_number VARCHAR(8)", "timestamp_ctr datetime", "timestamp datetime", "temperature int", "voltage int", "temperature_cpu int", "restart_number int", "cell_number VARCHAR(8)"]
+main_var = ["id INT AUTO_INCREMENT PRIMARY KEY", "object_number VARCHAR(8)", "timestamp_ctr datetime", "temperature int", "voltage int", "temperature_cpu int", "restart_number int", "cell_number VARCHAR(8)"]
 discrete_block_prefix = "input_state_"
-discrete_block_columns = [discrete_block_prefix + str(i) + " int" for i in range(1,33)]
+discrete_block_columns = [discrete_block_prefix + str(i+1) + " int" for i in range(0,8)]
 micom_registers = ["0140","0169","0165","002B","0111","005A","0026","0030","0032","0034","0036"]
 register_data_columns = ['reg_' + register + '_data int' for register in micom_registers]
 register_value_columns = ['reg_' + register + '_value int' for register in micom_registers]
@@ -35,7 +35,7 @@ regular_table_columns = main_var
 regular_table_columns += discrete_block_columns
 regular_table_columns += combine_data_value_columns
 
-create_table = "CREATE TABLE dreamline_regular_data ( " + ','.join(regular_table_columns) + ");"
+create_table = "CREATE TABLE dreamline_regular_data ( " + ','.join(regular_table_columns + ["timestamp datetime"]) + ");"
 print(create_table)
 # Insert new employee
 cursor.execute(create_table)
