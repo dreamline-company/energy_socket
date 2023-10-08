518b5318a2ea1542950ee352db754f1ea5657fe4
Amirkhan Orazbay <amirhan9878@gmail.com>
2023-10-08 16:45:42 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ docker stop dream-socket
 docker rm dream-socket
 docker image prune
 docker build -t dream-socket:latest .
-docker run -p 8070:8070 --restart unless-stopped -d --name dream-socket realsleep/dream-socket:latest
+docker run -p 8070:8070 --restart unless-stopped -d --name dream-socket dream-socket:latest
