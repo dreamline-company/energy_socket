cea82987ec3831fd439fb96e0d5ec45a732011f3
realsleep <amirhan9878@gmail.com>
2023-11-16 12:16:03 +0600

    gsefaesdfa

--- socket_data_parser.py
+++ socket_data_parser.py
@@ -102,7 +102,7 @@ def parse_emergency_data(base_data, main_data):
     data = {}
     data.update(base_data)
     for index, cell in enumerate(main_data, 1):
-        print(cell)
+        print(cell.decode().split(":"))
         s = cell.decode().split(":")
         data["c" + s[0]] = int(s[1])
     return data
