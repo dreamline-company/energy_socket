5993189a8fec5c169e05a6fb6fe4610591a0b92c
DarkhanMami <dmami@nu.edu.kz>
2024-03-08 22:13:13 +0500

    change timezone to aqtau

--- emg_socket.py
+++ emg_socket.py
@@ -228,7 +228,8 @@ ServerSocket.listen()
 # Цикл, где принимаем все соединения
 while True:
     # принимаем соединение от клиента
-    sleep(randint(1, 5))
+    # sleep(randint(1, 5))
+    sleep(1)
     Client, new_socket = ServerSocket.accept()
     Client.settimeout(60)
     print("Connection from: " + new_socket[0] + ":" + str(new_socket[1]))
